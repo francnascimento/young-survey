d64290215464826810aa5ffe3db9946e18fa62a8
Francisco Nascimento <francnascimento@users.noreply.github.com>
2017-11-14 19:19:19 -0200

    Update init.r

--- init.r
+++ init.r
@@ -14,6 +14,10 @@ survey["Slow music"] <- NULL
 survey["Fast music"] <- NULL
 survey["SmokingNumber"] <- NULL
 survey["AlcoholNumber"] <- NULL
+survey["Atrasado"] <- NULL
+survey["QuantidadeMentiras"] <- NULL
+survey["NivelEducacao"] <- NULL
+survey['TempoGastoInternet'] <- NULL
 
 #categorias
 musicas <- c("Music","Slow.music", "Fast.music", "Dance","Folk","Country","Classical.music","Musical","Pop","Rock","Metal.or.Hardrock","Punk","Hiphop..Rap","Reggae..Ska","Swing..Jazz","Rock.n.roll","Alternative","Latino","Techno..Trance","Opera")
@@ -22,10 +26,13 @@ hobbysEInteresses <- c("History","Psychology","Politics","Mathematics","Physics"
 fobias <- c("Flying","Storm","Darkness","Heights","Spiders","Snakes","Rats","Ageing","Dangerous.dogs","Fear.of.public.speaking")
 habitosAlimentares <- c("SmokingNumber", "AlcoholNumber", "Healthy.eating")
 habitosDeCompra <- c("Finances","Shopping.centres","Branded.clothing","Entertainment.spending","Spending.on.looks","Spending.on.gadgets","Spending.on.healthy.eating")
-personalidadeQuantitativo <- c("Daily.events","Prioritising.workload","Writing.notes","Workaholism","Thinking.ahead","Final.judgement","Reliability","Keeping.promises","Loss.of.interest","Friends.versus.money","Funniness","Fake","Criminal.damage","Decision.making","Elections","Self.criticism","Judgment.calls","Hypochondria","Empathy","Eating.to.survive","Giving","Compassion.to.animals","Borrowed.stuff","Loneliness","Cheating.in.school","Health","Changing.the.past","God","Dreams","Charity","Number.of.friends","Waiting","New.environment","Mood.swings","Appearence.and.gestures","Socializing","Achievements","Responding.to.a.serious.letter","Children","Assertiveness","Getting.angry","Knowing.the.right.people","Public.speaking","Unpopularity","Life.struggles","Happiness.in.life","Energy.levels","Small...big.dogs","Personality","Finding.lost.valuables","Getting.up","Interests.or.hobbies","Parents..advice","Questionnaires.or.polls")
+personalidadeQuantitativo <- c("Daily.events","Prioritising.workload","Writing.notes","Workaholism","Thinking.ahead","Final.judgement","Reliability","Keeping.promises","Loss.of.interest","Friends.versus.money","Funniness","Fake","Criminal.damage","Decision.making","Elections","Self.criticism","Judgment.calls","Hypochondria","Empathy","Eating.to.survive","Giving","Compassion.to.animals","Borrowed.stuff","Loneliness","Cheating.in.school","Health","Changing.the.past","God","Dreams","Charity", "QuantidadeMentiras", "Atrasado","Number.of.friends","Waiting","New.environment","Mood.swings","Appearence.and.gestures","Socializing","Achievements","Responding.to.a.serious.letter","Children","Assertiveness","Getting.angry","Knowing.the.right.people","Public.speaking","Unpopularity","Life.struggles","Happiness.in.life","Energy.levels","Small...big.dogs","Personality","Finding.lost.valuables","Getting.up","Interests.or.hobbies","Parents..advice","Questionnaires.or.polls", "TempoGastoInternet")
 personalidadeQualitativo  <- c("Punctuality","Lying","Internet.usage")
-demograficosQuantitativos <- c("Age","Height","Weight","Number.of.siblings")
+demograficosQuantitativos <- c("Age","Height","Weight","Number.of.siblings", "NivelEducacao")
 demograficosQualitativos  <- c("Gender","Left...right.handed","Education","Only.child","Village...town","House...block.of.flats")
+
+variaveisQuantitativas <- c(musicas, filmes, hobbysEInteresses, fobias, habitosAlimentares, habitosDeCompra, habitosDeCompra, demograficosQuantitativos, personalidadeQuantitativo)
+variaveisQualitativas <-c(personalidadeQualitativo, demograficosQualitativos)
 
 print(nrow(survey))
 for (row in 1:nrow(survey)) {
@@ -34,32 +41,63 @@ for (row in 1:nrow(survey)) {
   
   switch(as.character(survey[row, "Smoking"]),"never smoked"={survey[row, "SmokingNumber"] <- 1},
          "tried smoking"={survey[row, "SmokingNumber"] <- 2},
-         "former smoker"={survey[row, "SmokingNumber"] <- 4},
-         "current smoker"={survey[row, "SmokingNumber"] <- 5},
-         {survey[row, "SmokingNumber"] <- NaN}
+         "former smoker"={survey[row, "SmokingNumber"] <- 3},
+         "current smoker"={survey[row, "SmokingNumber"] <- 4},
+         {survey[row, "SmokingNumber"] <- ''}
   )
   
   switch(as.character(survey[row, "Alcohol"]),"never"={survey[row, "AlcoholNumber"] <- 1},
-         "social drinker"={survey[row, "AlcoholNumber"] <- 3},
-         "drink a lot"={survey[row, "AlcoholNumber"] <- 5},
-         {survey[row, "SmokingNumber"] <- NaN}
+         "social drinker"={survey[row, "AlcoholNumber"] <- 2},
+         "drink a lot"={survey[row, "AlcoholNumber"] <- 3},
+         {survey[row, "SmokingNumber"] <- ''}
+  )
+  
+  switch(as.character(survey[row, "Punctuality"]),"i am often early"={survey[row, "Atrasado"] <- 1},
+         "i am always on time"={survey[row, "Atrasado"] <- 2},
+         "i am often running late"={survey[row, "Atrasado"] <- 3},
+         {survey[row, "Atrasado"] <- ''}
+  )
+  
+  switch(as.character(survey[row, "Lying"]),"never"={survey[row, "QuantidadeMentiras"] <- 1},
+         "only to avoid hurting someone"={survey[row, "QuantidadeMentiras"] <- 2},
+         "sometimes"={survey[row, "QuantidadeMentiras"] <- 3},
+         "everytime it suits me"={survey[row, "QuantidadeMentiras"] <- 4},
+         {survey[row, "QuantidadeMentiras"] <- ''}
+  )
+  
+  switch(as.character(survey[row, "Internet.usage"]),"no time at all"={survey[row, "TempoGastoInternet"] <- 1},
+         "less than an hour a day"={survey[row, "TempoGastoInternet"] <- 2},
+         "few hours a day"={survey[row, "TempoGastoInternet"] <- 3},
+         "most of the day"={survey[row, "TempoGastoInternet"] <- 4},
+         {survey[row, "TempoGastoInternet"] <- ''}
+  )
+  
+  switch(as.character(survey[row, "Education"]),"primary school"={survey[row, "NivelEducacao"] <- 1},
+         "secondary school"={survey[row, "NivelEducacao"] <- 2},
+         "college/bachelor degree"={survey[row, "NivelEducacao"] <- 3},
+         "masters degree"={survey[row, "NivelEducacao"] <- 4},
+         {survey[row, "NivelEducacao"] <- ''}
   )
 }
 survey <- survey[complete.cases(survey),] #limpa missing data
+print(nrow(survey))
+
+surveyQuantitativo <- survey[,variaveisQuantitativas] #DataSet contendo somente colunas quantitativas
 
 #Matrizes de correlação
-corrplot(cor(survey[musicas]),diag=FALSE)
-corrplot(cor(survey[filmes]))
-corrplot(cor(survey[hobbysEInteresses]))
-corrplot(cor(survey[habitosAlimentares]))
-corrplot(cor(survey[fobias]))
-corrplot(cor(survey[habitosDeCompra]))
-corrplot(cor(survey[personalidadeQuantitativo]))
-corrplot(cor(survey[demograficosQuantitativos]))
+corrplot(cor(survey[musicas]), type = "lower", tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[filmes]), type = c("lower"), tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[hobbysEInteresses]), type = c("lower"), tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[habitosAlimentares]), type = c("lower"), tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[fobias]), type = c("lower"), tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[habitosDeCompra]), type = c("lower"), tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[personalidadeQuantitativo]), type = c("lower"), tl.srt = 45, diag = FALSE)
+corrplot(cor(survey[demograficosQuantitativos]), type = c("lower"), tl.srt = 45, diag = FALSE)
+
 
 #INÍCIO - Análise Fatorial
 #Etapas
-#1 - Análise de Matriz de correlação e Adequação da utilizaçãoda AF
+#1 - Análise de Matriz de correlação e Adequação da utilização da AF
 #2 - Extração dos fatores iniciais e Escolha dos numeros dos fatores
 #3 - Rotação dos Fatores
 #4 - Interpretação dos Fatores
@@ -96,11 +134,11 @@ corrplot(cor(survey[demograficosQuantitativos]))
 #- Cargas maiores de 0.30
 
 #Analise Descritiva
-summary(survey)
-sd(survey)
+summary(surveyQuantitativo)
+sd(surveyQuantitativo)
 
 #Análise dos componentes principais
-fit <- princomp(survey,cor=TRUE)
+fit <- princomp(surveyQuantitativo,cor=TRUE)
 #cor = TRUE, os componentes principais serão gerados a partir da matriz de correlação
 #caso cor = FALSE, os componentes principais serão gerados a partir da matriz de covariância
 
@@ -112,7 +150,7 @@ summary(fit)
 
 #Apresentação das Cargas Fatoriais considerando todos os vetores
 #Na função loadings podemos acrescentar a opção cutoff = 0.5, para que os loadings < 0.5 fiquem ocultos
-loadings(fit)
+loadings(fit, cutoff = 0.5)
 
 #Para analisar a quantidade de fatores suficientes à análise fatorial, deve-se realizar o scree plot
 plot(fit, type = "line")
@@ -128,12 +166,12 @@ biplot(fit) #consegue-se ver a angulação entre cada variável
 
 ##Análise fatorial a partir do pacote Psych
 #Análise de adequação
-#Teste de efericidade de Barlett
+#Teste de esferacidade de Barlett
 #Hipotese nula(H0): matriz de correlação entre variáveis é a matriz identidade.
 #ou seja, variáveis não são correlacionadas
-cortest.bartlett(survey[])
+cortest.bartlett(surveyQuantitativo)
 
-#Se H0 é rejeitada, a análse fatorial é valida
+#Se H0 é rejeitada, a análise fatorial é valida
 # p-value pequeno - rejeita-se H0, logo pode-se aplicar a análise fatorial
 
 
@@ -142,12 +180,12 @@ cortest.bartlett(survey[])
 #O método de rotação mais utilizado é processo VARIMAX,
 #que se trata de um método ortogonal de rotação que minimiza o número de variáveis com altas cargas sobre
 #um fator, reforçando assim a interpretabilidade dos fatores
-fit <- principal(survey[], nfactors=3, rotate="varimax", scores=TRUE)
+fit <- principal(surveyQuantitativo, nfactors=6, rotate="varimax", scores=TRUE)
 fit
 
 #plot factor 1 by factor 2
 load <- fit$loadings
-plot(load, type="n")
+plot(load)
 text(load, labels=names(survey[]),cex = 7) #adiciona nome das variáveis
 load
 
@@ -173,4 +211,4 @@ kmeans(survey[filmes], 3)
 distancias = dist(survey[filmes])
 h = hclust(distancias)
 plot(h)
-rect.hclust(h, 4)
+rect.hclust(h, 5)
